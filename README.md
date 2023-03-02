f3d2dab15bc81645021f4750a5bb6ab431f7d673
nahiyan <nahiyanmubashshir@gmail.com>
2023-03-03 00:09:11 +0600

    all files uploaded

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # DS-Task-1
+
+The objective of this project is to get better answers for user queries from gpt-3 on a specific matter. So, there can be some sectors, the data for those are not updated on gpt-3. To handle that, we tried to follow the following steps:
+
+First we'll read the data we want to use in a specific case.
+We will divide in to some chunks.
+Transform the chunks in to vector using embedding algorithm
+Save the vectors to a vector database.
+If an user query appears, we'll find some best matches. So, these are the steps we do s preparation of dataset. Then, If a query appeared, we do the following:
+We first take the query and find matches with the data we have on vector database, like a semantic serch.
+We take those contexts, and generate a prompt appropriate to the use case, including the contexts and the user's original question. We tell gpt-3 to answer based on the context.
+Note: The embedding model used here has 384 dimensions.
+
+Tasks:
+
+Load the text from the given docx file and split them in to some chunks. (A splitter is defined, you can use that.)
+Add all the splitted chunks to the vector database. (Use addData function)
+Create a prompt using the process discussed above.
+Get the answer from gpt-3 api.
+Get all the things together such that, we can pass a query using the function user_query and get a solid answer.
+The embedding model we used here is a basic embedding model, change the model and use openai's embedding model 'text-embedding-ada-002'
+Can we improve something in this process? Any suggestion you think of list it down.
+Do you think you have a better idea to handle the whole process? Write a summary about the alternative approach.
---
+++ main.py
@@ -0,0 +1,93 @@
+from sentence_transformers import SentenceTransformer, util
+import pinecone
+import openai
+import docx2txt
+
+# Load the SentenceTransformer model
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+text = docx2txt.process('DataLaw.docx')
+
+# Initialize Pinecone vector database
+pinecone.init(api_key="", environment="")
+index_name = "my_index"
+index = pinecone.Index(index_name)
+
+
+# Split text into smaller parts to be indexed separately
+def split_text_into_chunks(plain_text, max_chars=2000):
+    text_chunks = []
+    current_chunk = ""
+    for line in plain_text.split("\n"):
+        if len(current_chunk) + len(line) + 1 <= max_chars:
+            current_chunk += line + " "
+        else:
+            text_chunks.append(current_chunk.strip())
+            current_chunk = line + " "
+    if current_chunk:
+        text_chunks.append(current_chunk.strip())
+    return text_chunks
+
+
+# Add data to Pinecone index
+def addData(corpusData):
+    id = index.describe_index_stats()['total_vector_count']
+    for i in range(len(corpusData)):
+        chunk = corpusData[i]
+        chunkInfo = (str(id + i),
+                     model.encode(chunk).tolist(),
+                     {'context': chunk})
+        index.upsert(vectors=[chunkInfo])
+
+
+# Query Pinecone index and return matching contexts
+def find_match(query, k):
+    query_em = model.encode(query).tolist()
+    result = index.query(query_em, top_k=k, includeMetadata=True)
+    return [result['matches'][i]['metadata']['context'] for i in range(k)]
+
+
+# Generate a prompt based on the retrieved context and query
+def create_prompt(context, query):
+    prompt = f"Question: {query}\nContext: {context}\nAnswer:"
+    return prompt
+
+
+# Use GPT-3 to generate an answer to the prompt
+def generate_answer(prompt):
+    # Set up OpenAI API credentials
+    openai.api_key = "Insert openai API here"
+    model_engine = "text-davinci-002"  # Replace with your preferred GPT-3 model
+
+    # Generate answer using OpenAI's GPT-3 API
+    response = openai.Completion.create(
+        engine=model_engine,
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    answer = response.choices[0].text.strip()
+
+    return answer
+
+
+# Combine all the functions to provide a full user query experience
+def user_query(query, k=5):
+    # Search Pinecone index for matching contexts
+    matching_contexts = find_match(query, k)
+
+    # Generate prompts for each matching context and query
+    prompts = [create_prompt(context, query) for context in matching_contexts]
+
+    # Generate an answer to each prompt using GPT-3
+    answers = [generate_answer(prompt) for prompt in prompts]
+
+    # Return the matching contexts and their associated answers
+    return dict(zip(matching_contexts, answers))
+
+
+text_chunks = split_text_into_chunks(text)
+addData(text_chunks)
+user_query("How can I do this?")
